e128e3ae86bfa73338b62e917f9eb201bedfd430
PF-dev0ps <paulo.ferreyra@gmail.com>
2026-05-23 19:37:38 -0300

    Added: version-tag - Image reduced and multi cached build

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
-FROM python:3.10-slim
+FROM python:3.10-alpine AS Builder
 WORKDIR /app
-COPY app/requirements.txt .
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+
+FROM python:3.10-alpine
+WORKDIR /app
+COPY --from=builder /usr/local/lib/python3.10/site-packages /usr/local/lib/python3.10/site-packages
+COPY --from=builder /usr/local/bin /usr/local/bin
 COPY app/ .
 EXPOSE 8000
-CMD ["uvicorn","main:app","--host","0.0.0.0","--port","8000"]
+CMD ["gunicorn", "main:app", "--worker-class", "uvicorn.workers.UvicornWorker", "--access-logfile", "-", "--error-logfile", "-", "--capture-output", "--log-level", "info", "--bind", "0.0.0.0:8000"]
--- app/main.py
+++ app/main.py
@@ -1,30 +1,46 @@
-import json
-import hashlib
+import json    # Arreglos del input json (ordenar texto)
+import hashlib # Libreria para realizar el calculo el hash md5
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Request, Header #los campos necesarios para mas adelante 
+from pydantic import BaseModel, Field 
+from typing import Dict, Any
 
+app = FastAPI(title="Urbetrack MD5 challenge", description="API REST para validar MD5 sobre un JSON normalizado", version="1.0.3",)
 
-app = FastAPI(
-    title="Urbetrack MD5 challenge",
-    description="API REST para validar MD5 sobre un JSON normalizado",
-    version="1.0.3",    
-)
+class NormalizarRequest(BaseModel):
+    payload: Dict[str, Any] = Field(..., description="JSON normalizado a validar")
+    md5: str = Field(..., description="Valor MD5 esperado")
 
-class ValidateMD5Request(BaseModel):
-    payload: dict = Field(..., description="JSON a validar")
-    md5: str = Field(..., description="Valor MD5 esperado") 
-
+#                                 Endpoint 01 - HEALTH
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
-@app.post("/validate-md5")
-def validate_md5(request: ValidateMD5Request):
-    normalized_json = json.dumps(request.payload,sort_keys=True,separators=(',', ':'),ensure_ascii=False,)
-    calculated_md5 = hashlib.md5(normalized_json.encode('utf-8')).hexdigest()
+#                                 Endpoint 02 - JSON
+@app.post("/validar_mensaje")
+def validate_message(request: NormalizarRequest):
+    normalized_json = json.dumps(
+        request.payload,
+        sort_keys=True,
+        separators=(",", ":"),
+    )
+    calculated_md5 = hashlib.md5(normalized_json.encode("utf-8")).hexdigest()
 
     if calculated_md5 != request.md5.lower():
-        raise HTTPException(status_code=422, detail={"Error:":"MD5 mismatch","Expected": request.md5, "Calculated": calculated_md5,},)
+        return {"Valido": False, "MD5": calculated_md5}        
+    return {"Valido": True, "MD5": calculated_md5}
 
-    return {"Valido": True, "MD5": calculated_md5,}
+    
+
+
+# HASK OK: 8ddf45693d4185b95732d263fade0be2
+# "payload": {"empresa":"UrbeTrack","name":"Paulo"}
+# otro hash OK segun como se encripte: calculo_md5 487dd71db6ca994eafa617b7911406ae
+
+
+
+# Para probar:
+# curl -X POST http://localhost:8080/validar_md5 -H "Content-Type: application/json" -H "x-md5: 487dd71db6ca994eafa617b7911406ae" -d '{"payload": {"empresa":"UrbeTrack", "name":"Paulo"}}'
+
+
+
